941abd61a4475c83cccbceaf4ccbdf8962a2a200
ananthvk <123456789987654321sr@gmail.com>
2023-08-13 00:43:33 +0530

    Added coloring of output (only for the command)

--- cmd/imgur-cli.go
+++ cmd/imgur-cli.go
@@ -6,40 +6,90 @@ import (
 	"os"
 
 	"github.com/ananthvk/imgur-cli/internal"
+	"github.com/ananthvk/imgur-cli/internal/color"
 )
 
 var writer = flag.CommandLine.Output()
+var noColor = false
+
+func ColorError() {
+	if !noColor {
+		fmt.Fprint(writer, color.BrightRedBold)
+	}
+}
+
+func ColorHighlight() {
+	if !noColor {
+		fmt.Fprint(writer, color.WhiteBold)
+	}
+
+}
+
+func ColorSuccess() {
+	if !noColor {
+		fmt.Fprint(writer, color.GreenBold)
+	}
+
+}
+
+func ColorInfo() {
+	if !noColor {
+		fmt.Fprint(writer, color.BrightCyan)
+	}
+}
+
+func ColorReset() {
+	if !noColor {
+		fmt.Fprint(writer, color.Reset)
+	}
+}
 
 // Custom usage/help function
 func Usage() {
+	ColorSuccess()
 	fmt.Fprintf(writer, "imgur-cli: A CLI to upload images to imgur anonymously\n")
+	ColorReset()
+	ColorInfo()
 	fmt.Fprintf(writer, "Get the latest version at https://github.com/ananthvk/imgur-cli\n\n")
+	ColorReset()
+	ColorHighlight()
 	fmt.Fprintf(writer, "Usage:\n\n")
+	ColorReset()
 	flag.PrintDefaults()
 	/*
 		flag.VisitAll(func(f *flag.Flag) {
 			fmt.Fprintf(writer, " %v\n", f.Usage)
 		})
 	*/
-	fmt.Fprintf(writer, "The avaiable commands are:\n\n")
+	ColorHighlight()
+	fmt.Fprint(writer, "\nThe available commands are:\n\n")
+	ColorReset()
 	fmt.Fprintf(writer, "%s\t%s\n", "upload", "Uploads the given file(s) to imgur")
 	fmt.Fprintf(writer, "%s\t%s\n", "delete", "Delete the files specified by the delete hash(es)")
+	ColorHighlight()
 	fmt.Fprintf(writer, "\nExamples:\n\n")
+	ColorReset()
 	fmt.Fprintf(writer, "%s upload cat.png\n", os.Args[0])
 	fmt.Fprintf(writer, "%s delete <Delete hash>\n", os.Args[0])
 }
 
 // Tells the user to run the command with the help flag and exit
 func exitRunHelp() {
+	ColorInfo()
 	fmt.Fprintf(writer, "Run \"%s --help\" for help\n", os.Args[0])
+	ColorReset()
 	os.Exit(2)
 }
 
 func Run() {
 	flag.Usage = Usage
+	flag.BoolVar(&noColor, "no-color", false, "do not color the output")
+	_ = noColor
 	flag.Parse()
 	if len(flag.Args()) < 1 {
+		ColorError()
 		fmt.Fprint(writer, "No arguments provided\n")
+		ColorReset()
 		exitRunHelp()
 	}
 	args := flag.Args()
@@ -48,18 +98,24 @@ func Run() {
 	switch args[0] {
 	case "upload":
 		if len(args) == 1 {
+			ColorError()
 			fmt.Fprintf(writer, "No files provided to upload to imgur\n")
+			ColorReset()
 			exitRunHelp()
 		}
 		internal.Upload(args[1:])
 	case "delete":
 		if len(args) == 1 {
+			ColorError()
 			fmt.Fprintf(writer, "No delete hashes provided to delete from imgur\n")
+			ColorReset()
 			exitRunHelp()
 		}
 		internal.Delete(args[1:])
 	default:
+		ColorError()
 		fmt.Fprintf(writer, "%s : unknown command\n", args[0])
+		ColorReset()
 		exitRunHelp()
 	}
 }
---
+++ internal/color/colors.go
@@ -0,0 +1,45 @@
+package color
+
+const (
+	// Normal colors
+	Black   = "\033[0;30m"
+	Red     = "\033[0;31m"
+	Green   = "\033[0;32m"
+	Yellow  = "\033[0;33m"
+	Blue    = "\033[0;34m"
+	Magenta = "\033[0;35m"
+	Cyan    = "\033[0;36m"
+	White   = "\033[0;37m"
+
+	// Brighter colors
+	BrightBlack   = "\033[0;90m"
+	BrightRed     = "\033[0;91m"
+	BrightGreen   = "\033[0;92m"
+	BrightYellow  = "\033[0;93m"
+	BrightBlue    = "\033[0;94m"
+	BrightMagenta = "\033[0;95m"
+	BrightCyan    = "\033[0;96m"
+	BrightWhite   = "\033[0;97m"
+
+	// Bold colors
+	BlackBold   = "\033[1;30m"
+	RedBold     = "\033[1;31m"
+	GreenBold   = "\033[1;32m"
+	YellowBold  = "\033[1;33m"
+	BlueBold    = "\033[1;34m"
+	MagentaBold = "\033[1;35m"
+	CyanBold    = "\033[1;36m"
+	WhiteBold   = "\033[1;37m"
+
+	// Bright and bold colors
+	BrightBlackBold   = "\033[1;90m"
+	BrightRedBold     = "\033[1;91m"
+	BrightGreenBold   = "\033[1;92m"
+	BrightYellowBold  = "\033[1;93m"
+	BrightBlueBold    = "\033[1;94m"
+	BrightMagentaBold = "\033[1;95m"
+	BrightCyanBold    = "\033[1;96m"
+	BrightWhiteBold   = "\033[1;97m"
+
+	Reset = "\033[m"
+)
